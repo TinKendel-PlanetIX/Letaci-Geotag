5d64c536adcc63440857081957d63189c3bd549c
TinKendel-PlanetIX <tin.kendjel@planetix.eu>
2025-09-11 15:03:35 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,205 @@
+#include <exiv2.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cmath>  // For std::fabs
+#include <filesystem>
+
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string> split_csv(const std::string& line, char delimiter = ',')
+{
+    std::vector<std::string> result;
+    std::stringstream ss(line);
+    std::string token;
+
+    while (std::getline(ss, token, delimiter))
+    {
+        // This is done so that there is no whitespace or tabs in the token
+        token.erase(0, token.find_first_not_of("\t\r\n"));
+        token.erase(token.find_last_not_of("\t\r\n") + 1);
+        result.push_back(token);
+    }
+
+    return result;
+}
+
+std::string get_value(const std::vector<std::string> &row,
+                      const std::unordered_map<std::string, size_t> &column_index,
+                      const std::vector<std::string> &keys)
+{
+    for (auto& k : keys)
+    {
+        auto it = column_index.find(k);
+        if (it != column_index.end() && it->second < row.size())
+        {
+            return row[it->second];
+        }
+    }
+    return {};
+}
+
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Example usage: <image_folder> <csv_file.csv>" << '\n';
+    }
+
+
+    std::string folder_path = argv[1];       //"D:/Plesmo_Fotke";
+    std::string csv_file_path = argv[2];      //"D:/Plesmo_Fotke/CSV/KO_Plesmo_EVOSOPK.csv";
+
+    std::ifstream dataFile(csv_file_path);
+    if (!dataFile.is_open()) 
+    {
+        std::cerr << "Could not open file: " << csv_file_path << std::endl;
+        return 1;
+    }
+
+    std::string header_line {};
+    if (!std::getline(dataFile, header_line))
+    {
+        std::cerr << "CSV file is empty!\n";
+        return 1;
+    }
+
+    auto header_variables = split_csv(header_line, ',');
+    // This holds the names of variables that are in the header of the csv
+    // Example: file,	 lat,	 lon,	 altBaro,	 roll,	 pitch,	 yaw,	 time
+    std::unordered_map<std::string, size_t> column_index; 
+    for (size_t i = 0; i < header_variables.size(); i++)
+    {
+        std::string h = header_variables[i];
+        std::transform(h.begin(), h.end(), h.begin(), ::tolower);
+        column_index[h] = i;
+    }
+
+    std::string line;
+    while (std::getline(dataFile, line)) 
+    {
+        auto row = split_csv(line, ',');
+        if(row.empty())
+        {
+            continue;
+        }
+        
+        std::string image_name = get_value(row, column_index, {"imagename", "file"});
+        std::string latitude   = get_value(row, column_index, {"x", "lat"});
+        std::string longitude  = get_value(row, column_index, {"y", "lon"});
+        std::string altitude   = get_value(row, column_index, {"z", "altgps"});
+        std::string yaw        = get_value(row, column_index, {"omega", "yaw"});
+        std::string pitch      = get_value(row, column_index, {"phi", "pitch"});
+        std::string roll       = get_value(row, column_index, {"kappa", "roll"});
+        std::string time       = get_value(row, column_index, {"time"});
+    
+        if(image_name.empty())
+        {
+            continue;
+        }
+
+        std::string image_path = folder_path + "/" + image_name;
+        if (std::filesystem::exists(image_path)) 
+        {
+            try
+            {
+                auto image = Exiv2::ImageFactory::open(image_path);
+                image->readMetadata();
+
+                auto &exifData = image->exifData();
+                auto &xmpData  = image->xmpData();
+
+                // covert latitude before printing
+                if (!latitude.empty())
+                {
+                    double latitude_float = std::stod(latitude);
+                    uint32_t lat_deg = latitude_float;   // to int -> no decimal part
+                    double d_lat_min = 60.0 * (latitude_float - lat_deg);  // * decimal part
+                    uint32_t lat_min = d_lat_min;
+                    double d_lat_sec = 10000.0 * 60 * (d_lat_min - lat_min);
+                    uint32_t lat_sec = d_lat_sec;
+                    std::ostringstream lat;
+                    lat << lat_deg << "/1 " << lat_min << "/1 " << lat_sec << "/10000";
+
+                    exifData["Exif.GPSInfo.GPSVersionID"] = "2 3 0 0";
+                    exifData["Exif.GPSInfo.GPSLatitudeRef"] = "N";
+                    exifData["Exif.GPSInfo.GPSLatitude"] = lat.str();
+                }
+
+                if (!longitude.empty())
+                {
+                    // convert longitude before printing
+                    double longitude_float = std::stod(longitude);
+                    uint32_t lon_deg = longitude_float;   // to int -> no decimal part
+                    double d_lon_min = 60.0 * (longitude_float - lon_deg);  // * decimal part
+                    uint32_t lon_min = d_lon_min;
+                    double d_lon_sec = 10000.0 * 60 * (d_lon_min - lon_min);
+                    uint32_t lon_sec = d_lon_sec;
+                    std::ostringstream lon;
+                    lon << lon_deg << "/1 " << lon_min << "/1 " << lon_sec << "/10000";
+
+                    exifData["Exif.GPSInfo.GPSLongitudeRef"] = "E";
+                    exifData["Exif.GPSInfo.GPSLongitude"] = lon.str();
+                }
+
+
+                if (!altitude.empty())
+                {
+                    // convert altitude before printing
+                    double altitude_float = std::stod(altitude);
+                    uint32_t i_alt = 1000.0 * altitude_float;
+                    std::ostringstream alt;
+                    alt << i_alt << "/1000";
+
+                    exifData["Exif.GPSInfo.GPSAltitudeRef"] = "0";
+                    exifData["Exif.GPSInfo.GPSAltitude"] = alt.str();
+                }
+
+
+                if (!yaw.empty())
+                {
+                    xmpData["Xmp.drone-dji.GimbalYawDegree"] = yaw;
+                }
+
+
+                if (!pitch.empty())
+                {
+                    xmpData["Xmp.drone-dji.GimbalPitchDegree"] = pitch; 
+                }
+
+
+                if(!roll.empty())
+                {
+                    xmpData["Xmp.drone-dji.GimbalRollDegree"] = roll;
+                }
+
+
+                if(!time.empty())
+                {
+                    // do nothing for now
+                }
+
+                exifData["Exif.GPSInfo.GPSStatus"] = "A";
+                exifData["Exif.GPSInfo.GPSMapDatum"] = "WGS-84";
+
+                // Saving exif and xmp data to the image
+                image->setExifData(exifData);
+                image->setXmpData(xmpData);
+                image->writeMetadata();
+
+                std::cout << "Successfully geotagged: " << image_path << std::endl;
+            }
+            catch (const std::exception& e) 
+            {
+                std::cerr << "Error processing " << image_path << ": " << e.what() << std::endl;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ third_party/exiv2/include/exiv2.hpp
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef EXIV2_HPP_
+#define EXIV2_HPP_
+
+// *****************************************************************************
+// included header files
+#include "basicio.hpp"
+#include "bmffimage.hpp"
+#include "bmpimage.hpp"
+#include "config.h"
+#include "convert.hpp"
+#include "cr2image.hpp"
+#include "crwimage.hpp"
+#include "datasets.hpp"
+#include "easyaccess.hpp"
+#include "epsimage.hpp"
+#include "error.hpp"
+#include "exif.hpp"
+#include "futils.hpp"
+#include "gifimage.hpp"
+#include "http.hpp"
+#include "image.hpp"
+#include "iptc.hpp"
+#include "jp2image.hpp"
+#include "jpgimage.hpp"
+#include "metadatum.hpp"
+#include "mrwimage.hpp"
+#include "orfimage.hpp"
+#include "pgfimage.hpp"
+#include "photoshop.hpp"
+
+#ifdef EXV_HAVE_LIBZ
+#include "pngimage.hpp"
+#endif
+
+#include "preview.hpp"
+#include "properties.hpp"
+#include "psdimage.hpp"
+#include "rafimage.hpp"
+#include "rw2image.hpp"
+
+#include "tags.hpp"
+#include "tgaimage.hpp"
+#include "tiffimage.hpp"
+#include "types.hpp"
+#include "value.hpp"
+#include "version.hpp"
+#include "xmp_exiv2.hpp"
+#include "xmpsidecar.hpp"
+
+#endif  // ifndef EXIV2_HPP_
